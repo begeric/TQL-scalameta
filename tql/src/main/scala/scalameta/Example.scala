e4fb4bfc194ca9e53e82c7b53e1e954e389aca4a
begeric <begueteric@gmail.com>
2014-10-22 21:57:48 +0200

    Add some AllowedTransformation's

--- tql/src/main/scala/scalameta/Example.scala
+++ tql/src/main/scala/scalameta/Example.scala
@@ -26,10 +26,10 @@ object Example extends App{
   println(x)
 
   //val changeAllIntLits = deep(update{case q"${_ : Int}" => q"17"})
-  val changeAllIntLits = deep(update[Tree, Tree]{case Lit.Int(_) => q"197"})
+  val changeAllIntLits = deep(update[Lit, Lit]{case Lit.Int(_) => q"197"})
 
   println(getAllIntLits(x))
   println(changeAllIntLits(x))
 
-  println(x >> filter[Tree]{case Term.If(_,_,_) => true} \\ update[Tree, Tree]{case Lit.Int(_) => q"13"})
+  //println(x >> filter[Tree]{case Term.If(_,_,_) => true} \\ update[Lit, Lit]{case Lit.Int(_) => q"13"})
 }
--- tql/src/main/scala/scalameta/ScalaMetaTraverser.scala
+++ tql/src/main/scala/scalameta/ScalaMetaTraverser.scala
@@ -19,7 +19,16 @@ object ScalaMetaTraverser  extends Traverser[Tree] with Combinators[Tree] with S
   import MonoidEnhencer._
   import scala.reflect.{ClassTag, classTag}
 
-  implicit object tree2tree extends AllowedTransformation[Tree, Tree]
+  implicit object term2Term extends AllowedTransformation[Term, Term]
+  implicit object type2type extends AllowedTransformation[Type, Type]
+  implicit object decl2decl extends AllowedTransformation[Decl, Decl]
+  implicit object defn2defn extends AllowedTransformation[Defn, Defn]
+  implicit object lit2lit extends AllowedTransformation[Lit, Lit]
+  implicit object pat2pat extends AllowedTransformation[Pat, Pat]
+
+
+
+
 
 
   /*Those functions won't be here anymore soon*/
