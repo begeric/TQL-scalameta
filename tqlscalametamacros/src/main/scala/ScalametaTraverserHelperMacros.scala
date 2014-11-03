d8e0cae33ebffee3180be88291ab0a4ab15a5552
begeric <begueteric@gmail.com>
2014-11-03 16:42:26 +0100

    Fixed name for ScalametaTraverserHelperMacors => ScalametaTraverserHelperMacros

--- tqlscalametamacros/src/main/scala/ScalametaTraverserHelperMacros.scala
+++ tqlscalametamacros/src/main/scala/ScalametaTraverserHelperMacros.scala
@@ -8,7 +8,7 @@ import scala.language.experimental.macros
 import scala.reflect.macros.whitebox.Context
 
 
-object ScalametaTraverserHelperMacors {
+object ScalametaTraverserHelperMacros {
   def build[T, A](f: Any /*temporary*/, objs: Any*): (T => Option[(T, A)]) = macro ScalametaTraverserBuilder.buildImpl[T, A]
 
   def buildFromTopSymbol[T, A](f: Any): (T => Option[(T, A)]) = macro ScalametaTraverserBuilder.buildFromTopSymbol[T, A]
@@ -20,7 +20,23 @@ class ScalametaTraverserBuilder(override val c: Context) extends TraverserBuilde
 
   def buildFromTopSymbol[T : c.WeakTypeTag, A : c.WeakTypeTag](f: c.Tree): c.Tree = {
     u.symbolOf[T].asRoot.allLeafs.foreach(_.sym.owner.info) //weird hack so that the types are set in each symbol and the buildImpl function doesn't fail
-    val allLeafs = u.symbolOf[T].asRoot.allLeafs.map(x => q"${x.sym.companion}")
-    buildImpl[T, A](f, allLeafs: _*)
+
+    val allBranches = u.symbolOf[T].asRoot.allBranches.map{x =>
+        val parameter = TermName(c.freshName)
+        val funcName = TermName(c.freshName)
+        val func = q"""val $funcName = ${buildImpl[T, A](f, x.allLeafs.map(x => q"${x.sym.companion}"): _*)}"""
+        val cas = cq"$parameter @ (_ : ${x.sym}) => $funcName($parameter)"
+        (func, cas)
+    }
+
+    val parameter = TermName(c.freshName)
+    val (funcs, cases) = allBranches.unzip
+    q"""
+        ..$funcs
+        ($parameter: ${implicitly[c.WeakTypeTag[T]]}) => $parameter match {
+          case ..$cases
+          case v => Some((v, implicitly[Monoid[${implicitly[c.WeakTypeTag[A]]}]].zero))
+        }
+    """
   }
 }
