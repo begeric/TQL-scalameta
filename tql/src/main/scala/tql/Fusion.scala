18c3dd8f93a8377709e23c6432956a4bd8284f85
begeric <begueteric@gmail.com>
2014-12-21 22:50:55 +0100

    Transform the Fusion code into something more generic.

--- tql/src/main/scala/tql/Fusion.scala
+++ tql/src/main/scala/tql/Fusion.scala
@@ -1,27 +1,41 @@
 package tql
 
 import scala.language.higherKinds
-import scala.reflect._
 
 /**
  * Created by Eric on 21.12.2014.
  */
 trait Fusion[T] { self: Traverser[T] with Combinators[T] =>
 
-  class FusedTopDown[+A : Monoid](val m1: Matcher[A]) extends Matcher[A] {
+  /**
+   * Abstract class to allow two strategy to fuse.
+   * It is a system-f class in order to allow the fusion to different kind of strategy (down, up..) without
+   * having to duplicate the whole thing each time.
+   * */
+  abstract class Fused[A : Monoid, F[A] <: Fused[A, F]](val m1: Matcher[A]) extends Matcher[A] {
 
+    /**
+     * Exists to not have to use reflection with something like f.getClass.getConstructor[..]... inside compose
+     * */
+    def newInstance[B : Monoid](m: Matcher[B]): F[B]
+
+    /**
+     * strategy(a) + strategy(b) = strategy(a + b)
+     * */
     override def compose[B >: A : Monoid](m2: => Matcher[B]): Matcher[B] = m2 match {
-      case f: FusedTopDown[B] => new FusedTopDown[B](m1 compose f.m1)
+      case f: F[B] => newInstance(m1 compose f.m1)
       case _=> super.compose(m2)
     }
+  }
 
-    override def composeResults[B >: A : Monoid](m2: => Matcher[B]): Matcher[B] = m2 match {
-      case f: FusedTopDown[B] => new FusedTopDown[B](m1 composeResults f.m1)
-      case _=> super.composeResults(m2)
-    }
-
+  /**
+   * Create a fuser for the TopDown strategy (down)
+   * */
+  class FusedTopDown[A : Monoid](override val m1: Matcher[A]) extends Fused[A, FusedTopDown](m1) {
+    def newInstance[B : Monoid](m: Matcher[B]) = new FusedTopDown[B](m)
     def apply(t: T) = (m1 + children(this)).apply(t)
   }
 
   override def down[A : Monoid](m: Matcher[A]): Matcher[A] = new FusedTopDown[A](m)
+
 }
